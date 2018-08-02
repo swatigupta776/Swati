2046aff5309c4da71c4691c753f9e167af2f18cb
swatigupta776 <swati.gupta776@gmail.com>
2018-08-02 18:24:28 +0530

    first

--- Python Project/hello.py
+++ Python Project/hello.py
@@ -5,32 +5,32 @@ print("swati")
 a = 6
 b = 3
 c = a + b
-print (c)
+print(c)
 d = a * b
-print (d)
+print(d)
 e = a - b
-print (e)
+print(e)
 f = a / b
-print (f)
+print(f)
 g = a % b
-print (g)
+print(g)
 h = a ** b
-print (h)
+print(h)
 i = a + b * c
-print (i)
+print(i)
 j = (a + b) * (c + d)
-print (j)
+print(j)
 a = 'fshshj'
-print (a)
+print(a)
 a = a + a
-print (a)
+print(a)
 
 my_income = 100
 tax_rate = 0.1
 my_taxes = my_income * tax_rate
-print (my_taxes)
-print ('hi \n I am late')
-print ('hi\thello')
+print(my_taxes)
+print('hi \n I am late')
+print('hi\thello')
 p = len("I am swati")
 print(p)
 
@@ -39,16 +39,16 @@ newstring = string[-11]
 print(newstring)
 y = string.split('a')
 print(y)
-name ='Swati '
+name = 'Swati '
 h = name[2:]  # type: str
 print(h)
 
 q = "I"
 r = "am"
 s = "\nSwati"
-print(q,r,s)
+print(q, r, s)
 
-l = "w"+ h*10 +" " *10
+l = "w" + h * 10 + " " * 10
 print(l)
 
 pop = 'z' * 10
@@ -60,46 +60,46 @@ print(v)
 print("This is a string{}{}".format(' inserted', ' new '))
 
 result = 111.111
-test=999.9997
-#print(result)
-print('This is a result {1:15.3f} is right' .format(result, test))
+test = 999.9997
+# print(result)
+print('This is a result {1:15.3f} is right'.format(result, test))
 
-lista = (1,2,3)
-lista = (1.4,'string')
+lista = (1, 2, 3)
+lista = (1.4, 'string')
 print(len(lista))
 print(lista)
 print(type(lista))
 print(id(lista))
 
 print("-----------------")
-lista = (1,2,3)
-lista = (1.4,'string')
+lista = (1, 2, 3)
+lista = (1.4, 'string')
 print(len(lista))
 print(lista)
 print(type(lista))
 print(id(lista))
 
-lista = [1,2,3]
-lista = [1.4,'string']
+lista = [1, 2, 3]
+lista = [1.4, 'string']
 print(len(lista))
 print(lista)
 print(type(lista))
 print(id(lista))
 
 print("-----------------")
-lista = [1,2,3]
-lista = [1.4,'string']
+lista = [1, 2, 3]
+lista = [1.4, 'string']
 print(len(lista))
 print(lista)
 print(type(lista))
 print(id(lista))
 
-mylist = [1,2,3]
-mylist=['one','three']
+mylist = [1, 2, 3]
+mylist = ['one', 'three']
 new_list = lista + mylist
 print(new_list)
 
-mylist[0]= 0
+mylist[0] = 0
 print(mylist)
 
 mylist.append('two')
@@ -110,51 +110,51 @@ print(mylist)
 print(mylist.pop())
 print(mylist)
 
-sortlist = [9,3,5,1,6,0]
+sortlist = [9, 3, 5, 1, 6, 0]
 sortlist.sort()
 print(sortlist)
 print(type(sortlist))
 res = sortlist.reverse()
 print(res)
 
-my_dict ={'language1':'java','language2':'C'}
+my_dict = {'language1': 'java', 'language2': 'C'}
 print(my_dict)
 print(my_dict['language1'])
 
-versions ={'java':'3.2','C':'4.4'}
+versions = {'java': '3.2', 'C': '4.4'}
 print(versions['java'])
 
-d ={'k1':'123','k2':[1,2,3],'k3':{'insidekey':'100'}}
+d = {'k1': '123', 'k2': [1, 2, 3], 'k3': {'insidekey': '100'}}
 print(d['k2'][1])
 
-d = {'k1':['a','b','c']}
+d = {'k1': ['a', 'b', 'c']}
 nlist = d['k1']
 print(nlist)
 letter = nlist[1].upper()
-#up =letter.upper()
-#print(up)
+# up =letter.upper()
+# print(up)
 print(letter)
-d = {'k1':'100','k2':'200'}
+d = {'k1': '100', 'k2': '200'}
 d['k3'] = 300
 print(d)
-d['k1']= 'NEW VALUE'
+d['k1'] = 'NEW VALUE'
 print(d)
 print(d.keys())
 print(d.values())
 print(d.items())
 
-#t = ('one',3)
-#t[1]
-#print(t[1])
+# t = ('one',3)
+# t[1]
+# print(t[1])
 
-l = [1,2,3]
+l = [1, 2, 3]
 print(l[-1])
 type(l)
 print(type(l))
-l[0] ='new'
+l[0] = 'new'
 print(l)
 
-t=('a','a','b')
+t = ('a', 'a', 'b')
 
 print(t.count('a'))
 
@@ -163,9 +163,8 @@ myset = set()
 myset.add(5)
 myset.add(2)
 print(myset)
-melist = [1,1,1,7,7,6,6]
+melist = [1, 1, 1, 7, 7, 6, 6]
 print(set(melist))
-
 
 people = set('Jay')
 people.add('Daxit')
@@ -173,12 +172,12 @@ people.add('mit')
 print(people)
 
 qw = 1
-qe =2
-ad = qw>qe
+qe = 2
+ad = qw > qe
 print(ad)
 
-b= none
+b = none
 print(b)
 
-f = open('F:/file.txt')
+f = open('F://file.txt')
 f.write('Python')
